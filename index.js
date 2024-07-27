a236d5fc5d8bc5cd6aca1ebe282d933048760bfd
enkeljon gjeta <enkelionimarku@gmail.com>
2024-07-27 18:43:38 +0200

    added the spline and gona wrap up soon

--- index.js
+++ index.js
@@ -36,3 +36,66 @@ gsap.from("#text1", {
     opacity: 0,
     color: "white"
 })
+
+gsap.from(".present", {
+    scrollTrigger:{
+        trigger: ".present",
+        start: "-20% center",
+        end: "center 40%",
+        markers: true,
+        toggleActions: "restart complete reverse reset",
+    },
+    duration: 2,
+    backgroundColor: "#83B4FF"
+})
+
+gsap.to(".container", {
+    scrollTrigger: {
+        trigger: ".present",
+        start: "-20% center",
+        end: "center 40%",
+        markers: true,
+        toggleActions: "restart complete reverse reset",
+    },
+    duration: 2,
+    backgroundColor: " rgb(14, 17, 35)"
+})
+
+gsap.from(".first", {
+    scrollTrigger:{
+        trigger: ".first",
+        start: "-20% center",
+        end: "center 40%",
+        markers: true,
+        toggleActions: "restart complete reverse reset",
+    },
+    duration: 3,
+    y:200,
+    opacity: 0
+})
+
+gsap.from(".sec", {
+    scrollTrigger:{
+        trigger: ".sec",
+        start: "-20% center",
+        end: "center 40%",
+        markers: true,
+        toggleActions: "restart complete reverse reset",
+    },
+    duration: 3,
+    y:200,
+    opacity: 0
+})
+
+gsap.from(".third", {
+    scrollTrigger:{
+        trigger: ".third",
+        start: "-20% center",
+        end: "center 40%",
+        markers: true,
+        toggleActions: "restart complete reverse reset",
+    },
+    duration: 3,
+    y:200,
+    opacity: 0
+})
